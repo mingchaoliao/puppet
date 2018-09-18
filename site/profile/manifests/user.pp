9aaf85369644497a7cc8cb3888cabe050707e214
Mingchao Liao <mingchaoliao95@gmail.com>
2018-09-18 13:48:12 -0400

    fix create user

--- site/profile/manifests/user.pp
+++ site/profile/manifests/user.pp
@@ -2,12 +2,14 @@ class profile::user (
   $users = {}
 ) {
   # TODO: fix this
+
   $users.each |Hash $user| {
     user { $user['username']:
-      ensure  => present,
-      comment => $user['full_name'],
-      expiry  => absent,
-      home    => "/home/${user['username']}"
+      ensure     => present,
+      comment    => $user['full_name'],
+      expiry     => absent,
+      home       => "/home/${user['username']}",
+      managehome => true
     }
 
     $user['public_keys'].each |Hash $public_key| {
@@ -20,7 +22,7 @@ class profile::user (
     }
 
     if($user['sudo'] == true) {
-      sudo::conf { "no-password sudoer ${user['username']}":
+      sudo::conf { "no_password_sudoer_${user['username']}":
         content => "${user['username']} ALL=(ALL) NOPASSWD:ALL",
       }
     }
