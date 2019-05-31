c7b3d20fbd57fef62146abc927c45995175b6d9c
Mingchao Liao <liaom@miamioh.edu>
2019-05-31 18:18:20 -0400

    create udesk

---
+++ site/profile/manifests/ssr.pp
@@ -0,0 +1,17 @@
+class profile::ssr(
+  $ensure   = 'present',
+  $source   = 'puppet:///files/ssr.deb'
+) {
+  file {'/tmp/puppet/ssr.deb':
+    ensure => $ensure,
+    source => $source
+  }
+  -> package {['libsodium18', 'libsodium-dev']:
+    ensure => $ensure
+  }
+  ->package {'ssr':
+    provider => 'dpkg',
+    ensure => $ensure,
+    source => '/tmp/puppet/ssr.deb'
+  }
+}
---
+++ site/role/manifests/linux/ubuntu/udesk.pp
@@ -0,0 +1,10 @@
+class role::linux::ubuntu::udesk {
+  contain role::linux::ubuntu::desktop
+  contain profile::applications::chrome
+  contain profile::ssr
+
+  exec {'baidupcs config set -enable_https=true -savedir=/media/localu -max_download_load=5 -max_parallel=200':
+    user => 'liaom',
+    require => [Class[profile::baidupcs]]
+  }
+}
