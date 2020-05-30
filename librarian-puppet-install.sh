e5891388e52ea810ccba5aa51ee3891de6e83f08
Mingchao Liao <mingchaoliao95@gmail.com>
2020-05-29 23:54:12 -0400

    fix some warnings

--- librarian-puppet-install.sh
+++ librarian-puppet-install.sh
@@ -2,4 +2,20 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-sudo bash -l -c "cd $DIR && librarian-puppet install"
+CMD="install"
+VERBOSE=""
+
+while getopts ":vu" opt; do
+  case ${opt} in
+    v ) # process option h
+      VERBOSE='--verbose'
+      ;;
+    u ) # process option t
+      CMD='update'
+      ;;
+    \? ) echo "Usage: ./librarian-puppet-install.sh [-v] [-u]"
+      ;;
+  esac
+done
+
+sudo bash -l -c "cd $DIR && librarian-puppet $CMD $VERBOSE"
--- site/profile/manifests/docker/command.pp
+++ site/profile/manifests/docker/command.pp
@@ -2,12 +2,12 @@ define profile::docker::command(
   $ensure = 'present',
   $commandName   = $title,
   $image,
-  $tag    = 'latest',
+  $imageTag    = 'latest',
   $cmd
 ) {
   file {"/opt/docker_cmds/${commandName}":
     ensure => $ensure,
-    content => sprintf('docker run --rm -it --user="$(id -u):$(id -g)" -v "/etc/passwd:/etc/passwd:ro" -v "${HOME}:${HOME}" --workdir="/src" -v "$(pwd):/src" %s:%s %s $@', $image, $tag, $cmd),
+    content => sprintf('docker run --rm -it --user="$(id -u):$(id -g)" -v "/etc/passwd:/etc/passwd:ro" -v "${HOME}:${HOME}" --workdir="/src" -v "$(pwd):/src" %s:%s %s $@', $image, $imageTag, $cmd),
     mode => '0755'
   }
 }
