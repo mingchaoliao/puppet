9bea4729cafda28e48a08f93a4ece2ea38743f2a
Mingchao Liao <liaom@ohio.edu>
2021-12-19 00:32:21 -0500

    update

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -13,3 +13,4 @@ sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo groupadd docker
 sudo usermod -aG docker "$(whoami)"
 sudo newgrp docker
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
