46ef8ec601866ed6702083e28c20ea5b6626e736
Mingchao Liao <liaom@ohio.edu>
2022-02-18 13:45:44 -0500

    update

--- site/role/manifests/oit.pp
+++ site/role/manifests/oit.pp
@@ -3,6 +3,7 @@ class role::oit {
   contain profile::skaffold
   contain profile::oit::dcvpn
   contain profile::oit::keep_mouse_moving
+  contain profile::oit::run_node_resource
 
   package {[
     'teams'
