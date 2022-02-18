d0a7ae0e5029c647e2c225f89f0954fdbd6cf6c4
Mingchao Liao <liaom@ohio.edu>
2022-02-18 13:44:07 -0500

    update

--- site/profile/manifests/oit/dcvpn.pp
+++ site/profile/manifests/oit/dcvpn.pp
@@ -7,7 +7,7 @@ class profile::oit::dcvpn(
       ensure => $ensure
     }
     -> Exec {'Add DCVPN':
-      command => "/usr/bin/nmcli c add type vpn con-name oit-dcvpn vpn-type l2tp vpn.data 'user=$user, gateway=dcvpn.ohio.edu, ipsec-enabled=yes, ipsec-esp=aes256-sha1, ipsec-ike=aes256-sha1-ecp384, ipsec-psk=ohiobobcats, password-flags=0'",
+      command => "/usr/bin/nmcli c add type vpn con-name oit-dcvpn vpn-type l2tp vpn.data 'user=$user, gateway=dcvpn1.ohio.edu, ipsec-enabled=yes, ipsec-esp=aes256-sha1, ipsec-ike=aes256-sha1-ecp384, ipsec-psk=ohiobobcats, password-flags=0'",
       unless => '/usr/bin/nmcli c | grep oit-dcvpn'
     }
   } else {
