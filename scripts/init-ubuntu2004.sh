7e3449c292894f523fb1274dcfc9afb8de891967
Mingchao Liao <liaom@ohio.edu>
2023-11-25 15:03:42 -0500

    update

---
+++ scripts/init-ubuntu2004.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root."
+  exit
+fi
+
+apt-get update && apt-get install -y wget
+
+cd /tmp || exit 1
+
+wget https://apt.puppet.com/puppet8-release-focal.deb \
+  && dpkg -i puppet8-release-focal.deb \
+  && apt-get update \
+  && apt-get install -y puppet-agent \
+  && echo "source /etc/profile.d/puppet-agent.sh" >> ~/.bashrc
+
+apt-get install -y unzip git \
+  && wget https://github.com/xorpaul/g10k/releases/download/v0.9.8/g10k-linux-amd64.zip \
+  && unzip g10k-linux-amd64.zip \
+  && mv g10k /usr/local/bin
+
+cd - || exit 1
