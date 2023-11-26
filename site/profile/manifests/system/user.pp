1bda6a33bdcb0afd202478688c2a3c9fd2dce5ae
Mingchao Liao <liaom@ohio.edu>
2023-11-25 20:36:35 -0500

    update

--- site/profile/manifests/system/user.pp
+++ site/profile/manifests/system/user.pp
@@ -19,28 +19,6 @@ class profile::system::user (
         managehome => true,
       }
 
-      if $user['ensure'] != 'absent' {
-        file { "bashrc_${username}":
-          ensure => 'file',
-          path => ($username == 'root' ? {
-            true  => '/root/.bashrc',
-            false => "/home/$username/.bashrc",
-          }),
-          require => User[$username],
-        }
-        -> file_line { "bash_profile_${username}":
-          ensure => 'bash_profile' in $user ? {
-            true  => $user['bash_profile'],
-            false => 'absent',
-          },
-          path   => ($username == 'root' ? {
-            true  => '/root/.bashrc',
-            false => "/home/$username/.bashrc",
-          }),
-          line   => "source /opt/bash-profile/main",
-        }
-      }
-
       if($user['ensure'] != 'absent') {
         $user['public_keys'].each |Hash $public_key| {
           ssh_authorized_key { $public_key['name']:
@@ -57,6 +35,28 @@ class profile::system::user (
           ensure => $user['ensure'],
           content => "$username ALL=(ALL) NOPASSWD:ALL",
         }
+      }
+    }
+
+    if $user['ensure'] != 'absent' {
+      file { "bashrc_${username}":
+        ensure => 'file',
+        path => ($username == 'root' ? {
+          true  => '/root/.bashrc',
+          false => "/home/$username/.bashrc",
+        }),
+        require => User[$username],
+      }
+      -> file_line { "bash_profile_${username}":
+        ensure => 'bash_profile' in $user ? {
+          true  => $user['bash_profile'],
+          false => 'absent',
+        },
+        path   => ($username == 'root' ? {
+          true  => '/root/.bashrc',
+          false => "/home/$username/.bashrc",
+        }),
+        line   => "source /opt/bash-profile/main",
       }
     }
   }
